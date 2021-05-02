4446ad079066e768b0557bb361d36af8dea12efe
vpravd1@gmail.com <Vv77100369>
2021-05-02 22:35:24 +0300

    1

---
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonaerolines/Destinations.java
@@ -0,0 +1,18 @@
+package by.jwd.pravdivets.task6.karlssonaerolines;
+
+public enum Destinations {
+
+	MOSCOW(728),
+	PARIS(2114),
+	NEW_YORK(7111),
+	BIJING(6472),
+	URUPINSK(1346);
+	
+	int distance;
+	
+	Destinations(int distance){
+		this.distance = distance;
+	}
+	
+	
+}
---
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonaerolines/PlaneModels.java
@@ -0,0 +1,17 @@
+package by.jwd.pravdivets.task6.karlssonaerolines;
+
+public enum PlaneModels {
+
+	BOEING_377,
+	BOEING_737,
+	BOEING_747,
+	AIRBUS_A320,
+	AIRBUS_A330,
+	AIRBUS_A340,
+	AIRBUS_A380,
+	//https://ru.wikipedia.org/wiki/Airbus
+	TU_204,
+	TU_214;
+	
+	
+}
