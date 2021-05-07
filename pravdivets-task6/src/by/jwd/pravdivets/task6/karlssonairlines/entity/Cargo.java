0ea1a316834575c76383d20dfb18d11852948951
vpravd1@gmail.com <Vv77100369>
2021-05-07 11:38:14 +0300

    1

--- pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/Cargo.java
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/Cargo.java
@@ -2,10 +2,20 @@ package by.jwd.pravdivets.task6.karlssonairlines.entity;
 
 public abstract class Cargo {
 	
+	private String name;
 	private int weight;
 	
-	public Cargo (int weight) {
+	public Cargo (String name, int weight) {
+		this.name = name;
 		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {//вроде как заново маркировать товар
+		this.name = name;
 	}
 
 	public int getWeight() {
