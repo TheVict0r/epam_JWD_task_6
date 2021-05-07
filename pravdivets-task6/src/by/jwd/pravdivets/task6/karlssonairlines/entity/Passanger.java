0ba1939a951432f6f4e38a9db4ec88947cc7d3ba
vpravd1@gmail.com <Vv77100369>
2021-05-07 15:38:14 +0300

    1

---
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/Passanger.java
@@ -0,0 +1,15 @@
+package by.jwd.pravdivets.task6.karlssonairlines.entity;
+
+import java.io.Serializable;
+
+public class Passanger extends CargoUnit implements Serializable{
+
+	
+	private int luggageWeight;
+	boolean businessClass;
+	
+	
+	
+	
+	
+}
