d38217bbceea9ef1b01ebfd082bd1d8e0daf92fb
vpravd1@gmail.com <Vv77100369>
2021-05-08 17:18:39 +0300

    1

--- pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/Passenger.java
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/Passenger.java
@@ -5,68 +5,38 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Passenger extends CargoUnit implements Serializable{
 
-	private int luggageWeight; //багаж будем учитывать вместе с пассажиром. С точки зрения авиакомпании прописывать отдельно смысла нет. 
+	//вес багажа будем учитывать вместе с пассажиром. С точки зрения авиакомпании прописывать багаж отдельно смысла нет. 
 	boolean businessClass;
 	
 	public Passenger() { }
 	
-	public Passenger(int id, int weight, int luggageWeight) {
+	public Passenger(int id, int weight, boolean businessClass) {
 		super(id, weight);
-		this.luggageWeight = luggageWeight;
+		
 		this.businessClass = businessClass;
 	}
 
-	public int getLuggageWeight() {
-		return luggageWeight;
-	}
 
 	public boolean isBusinessClass() {
 		return businessClass;
-	}
-
-	public void setLuggageWeight(int luggageWeight) {
-		this.luggageWeight = luggageWeight;
 	}
 
 	public void setBusinessClass(boolean businessClass) {
 		this.businessClass = businessClass;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = super.hashCode();
-//		result = prime * result + (businessClass ? 1231 : 1237);
-//		result = prime * result + luggageWeight;
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (!super.equals(obj))
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Passanger other = (Passanger) obj;
-//		if (businessClass != other.businessClass)
-//			return false;
-//		if (luggageWeight != other.luggageWeight)
-//			return false;
-//		return true;
-//	}
-	
 	@Override
 	public boolean equals(Object o) {
-		if (!super.equals(o)) {
+		if(this == o) {
+			return true;
+		}
+		if(this.getClass() != o.getClass()) {
+			return false;
+		}
+		if(!super.equals(o)) {
 			return false;
 		}
 		Passenger passanger = (Passenger) o;
-
-		if (this.luggageWeight != passanger.luggageWeight) {
-			return false;
-		}
 
 		if (this.businessClass != passanger.businessClass) {
 			return false;
@@ -76,13 +46,17 @@ public class Passenger extends CargoUnit implements Serializable{
 
 	@Override
 	public int hashCode() {
-		final int meaning = 42;
+		final int prime = 31;
 		int result = super.hashCode();
-		result = result * meaning + luggageWeight;
+		result = prime * result + (businessClass ? 1221 : 1239);
+		return result;
 	}
 	
 	
-	
+	@Override
+	public String toString() {
+		return super.toString() + (businessClass ? "business class" : "economy class") + "]";
+	}
 	
 	
 }
