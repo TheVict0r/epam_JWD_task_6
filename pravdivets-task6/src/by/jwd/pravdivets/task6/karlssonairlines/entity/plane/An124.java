fd0b1fe848614c8a50ab24b0e248b2f1947f00cd
vpravd1@gmail.com <Vv77100369>
2021-05-09 12:07:41 +0300

    1

--- pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/plane/An124.java
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/entity/plane/An124.java
@@ -7,13 +7,10 @@ import by.jwd.pravdivets.task6.karlssonairlines.entity.cargo.Shipment;
 @SuppressWarnings("serial")
 public class An124 extends CargoAircraft implements Serializable {
 
-	public An124() {
-		
-	}
+	public An124() { }
 
 	public An124(int id, int manufactYear, Shipment shipment) {
 		super(id, manufactYear, 120_000, 5400, 825, 12600, shipment);
-		
 	}
 
 	@Override
