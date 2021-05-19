9f1e4778c36d8b9e2ddac649845a2454227dcf5a
vpravd1@gmail.com <Vv77100369>
2021-05-19 23:51:17 +0300

    ready

--- pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/dao/impl/FileDataLoader.java
+++ pravdivets-task6/src/by/jwd/pravdivets/task6/karlssonairlines/dao/impl/FileDataLoader.java
@@ -1,21 +1,40 @@
 package by.jwd.pravdivets.task6.karlssonairlines.dao.impl;
 
 import by.jwd.pravdivets.task6.karlssonairlines.dao.PlanesDataLoader;
-import by.jwd.pravdivets.task6.karlssonairlines.dao.FileReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import by.jwd.pravdivets.task6.karlssonairlines.exception.DaoException;
 
 public class FileDataLoader implements PlanesDataLoader {
 
+	@SuppressWarnings("resource")
 	@Override
 	public String loadData(String source) throws DaoException {
 		if (source == null) {
-			throw new DaoException("The source is not provided");
+			throw new DaoException("The source was not provided");
 		}
 		
-		String result;
-		result = FileReader.readFile(source);
-		return result;
+		File file = new File(source);
 
+		StringBuilder builder = new StringBuilder();
+
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			throw new DaoException("The data file not found", e);
+		}
+
+		while (scanner.hasNext()) {
+			builder.append(scanner.nextLine());
+		}
+
+		String result = new String(builder);
+
+		return result;
 	}
 
 }
